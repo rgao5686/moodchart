5f9e2bd98c28a434f5456d4a2ee70a105794e076
Richard Gao <richardgao@192-168-1-105.tpgi.com.au>
2018-12-27 19:46:41 +1100

    Updates

---
+++ precache-manifest.89db4546dde5bd4ab1d7d16f4c3f1d57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54109f41710abb8f3974",
+    "url": "/moodchart/static/js/runtime~main.54109f41.js"
+  },
+  {
+    "revision": "cf3f022c6e8a14ba4759",
+    "url": "/moodchart/static/js/main.cf3f022c.chunk.js"
+  },
+  {
+    "revision": "bb530561083cd61ac79d",
+    "url": "/moodchart/static/js/1.bb530561.chunk.js"
+  },
+  {
+    "revision": "cf3f022c6e8a14ba4759",
+    "url": "/moodchart/static/css/main.7b4d2a09.chunk.css"
+  },
+  {
+    "revision": "bb530561083cd61ac79d",
+    "url": "/moodchart/static/css/1.742fed1b.chunk.css"
+  },
+  {
+    "revision": "0ab34b99b80453497a957adda36578a4",
+    "url": "/moodchart/index.html"
+  }
+];
